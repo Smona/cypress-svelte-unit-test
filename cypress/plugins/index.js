19c29640905a60ed3bba148a1cf0c767f08466d9
Mason Bourgeois <mason.bourgeois@gmail.com>
2019-07-14 17:17:38 -0500

    Add Sapper support
    Uses `webpackConfig.client` if it exists, making this package compatible with Sapper's webpack configuration.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,15 @@
 const webpack = require('@cypress/webpack-preprocessor')
+const webpackConfig = require('../webpack.config.js')
+
+// Sapper support. Sapper's webpack config exports multiple configs as
+// { client, server, serviceWorker }. Here we want to use the client config.
+const webpackOptions = webpackConfig.client || webpackConfig;
+
 module.exports = on => {
   const options = {
     // send in the options from your webpack.config.js, so it works the same
     // as your app's code
-    webpackOptions: require('../webpack.config.js')
+    webpackOptions 
   }
 
   on('file:preprocessor', webpack(options))
